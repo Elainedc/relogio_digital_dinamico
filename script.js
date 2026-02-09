d06b7384403244fe2923d389570b517558dd59e4
Elainedc <diascarvalhoelaine@gmail.com>
2026-02-09 17:23:34 -0300

    terminando js

--- script.js
+++ script.js
@@ -2,18 +2,28 @@ const hoursElement = document.getElementById('hours')
 const minutesElement = document.getElementById('minutes')
 const secondsElement = document.getElementById('seconds')
 
-const date = new Date();
+function newTime() {
 
-const hours = date.getHours();
-const minutes = date.getMinutes();
-const seconds = date.getSeconds();
+    const date = new Date();
 
-console.log(hours, minutes, seconds)
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
 
-hoursElement.textContent = fixTime(hours)
-minutesElement.textContent = fixTime(minutes)
-secondsElement.textContent = fixTime(seconds)
+    console.log(hours, minutes, seconds)
+
+    hoursElement.textContent = fixTime(hours)
+    minutesElement.textContent = fixTime(minutes)
+    secondsElement.textContent = fixTime(seconds)
+
+}
+
 
 function fixTime(time){
     return time < 10 ? '0'+time : time;
 }
+
+newTime()
+setInterval(() =>{
+    console.log('alo')
+}, 1000)
