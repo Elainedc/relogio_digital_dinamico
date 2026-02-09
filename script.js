edfd1719c6c92fff83792e460d6703ba41c69e1c
Elainedc <diascarvalhoelaine@gmail.com>
2026-02-09 17:14:01 -0300

    time

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const hoursElement = document.getElementById('hours')
 const minutesElement = document.getElementById('minutes')
 const secondsElement = document.getElementById('seconds')
+
+const date = new Date();
+
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+
+console.log(hours, minutes, seconds)
+
+hoursElement.textContent = fixTime(hours)
+minutesElement.textContent = fixTime(minutes)
+secondsElement.textContent = fixTime(seconds)
+
+function fixTime(time){
+    return time < 10 ? '0'+time : time;
+}
